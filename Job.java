dc4f28575e0406a75b9c4c3403b2193dcad5ce57
pyt <pm132010uk@gmail.com>
2015-10-13 14:18:08 +0100

    added doc comments to MyTask

--- Job.java
+++ Job.java
@@ -1,9 +1,11 @@
+//what m i doing
+
 public class Job implements Runnable {
   private int jobNumber;
   Job (int jobNumber) {
     this.jobNumber = jobNumber;
   }
-
+  
   public void run () {
   // Undertake required work, here we will emulate it by sleeping for a period
     System.out.println ("Job: " + jobNumber + " is being processed by thread : "
